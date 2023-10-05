d938a546de37942f543dacd57d1483f9112c1b15
Luca Fonzo <lucafonzo3110@gmail.com>
2023-10-05 16:58:48 -0300

    Add progress text

--- TP2/js/spinner.js
+++ TP2/js/spinner.js
@@ -1,17 +1,23 @@
 function redirectToAnotherPage() {
   // Display the loading animation
   const loader = document.querySelector('.loader');
+  const percentage = document.querySelector('.progress-text span')
   loader.style.display = 'block';
+  document.querySelector('.container-loader').style.display = 'block';
   document.querySelector('form').style.visibility = 'hidden';
+  let progress = 0;
+  const interval = setInterval(function () {
+    progress++;
+    percentage.textContent = `${progress}%`;
 
-  // Simulate a delay (you can replace this with your actual loading process)
-  setTimeout(function () {
-    // Hide the loading animation
-    loader.style.display = 'none';
-
-    // Redirect to another page
-    window.location.href = 'home.html'; // Replace with the URL of the other page
-  }, 2000); // Adjust the delay as needed (2 seconds in this example)
+    if (progress === 100) {
+      clearInterval(interval); // Stop the interval when progress reaches 100%
+      setTimeout(function () {
+        loader.style.display = 'none'; // Hide the loading animation
+        window.location.href = 'home.html'; // Redirect to another page
+      }, 500); // Delay before redirecting (0.5 seconds in this example)
+    }
+  }, 20); // Adjust the interval and speed as needed
 }
 
 document.querySelector('#btn-login').addEventListener('click', (e) => {
