735d7a822e519ef90025b3b544e5ec5c4554386e
Luca Fonzo <lucafonzo3110@gmail.com>
2023-10-07 12:58:27 -0300

    Add carrousel para desktop

--- TP2/js/app.js
+++ TP2/js/app.js
@@ -33,3 +33,34 @@ btnsDesplegable.forEach((btn,index) => {
   });
 })
 
+
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+
+let currentIndex = 0;
+
+prevBtn.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateCarousel();
+  }
+});
+
+nextBtn.addEventListener('click', () => {
+  console.log(currentIndex)
+  if (currentIndex == 2) {
+    return;
+  }
+  if (currentIndex < carousel.children.length - 1) {
+    currentIndex++;
+    updateCarousel();
+  }
+});
+
+// Función para actualizar la posición del carrusel
+function updateCarousel() {
+  const translateX = -currentIndex * (280 * 3); // 280px es el ancho de cada tarjeta
+  carousel.style.transform = `translateX(${translateX}px)`;
+}
+
