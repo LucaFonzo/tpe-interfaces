c402c8e2396448bfcc8e5f428591f36e0d7fd964
Camilo Cagliolo <ccagliolo@alumnos.exa.unicen.edu.ar>
2023-10-07 21:10:29 -0300

    Añadido overlay al menú de navegación

--- TP2/js/app.js
+++ TP2/js/app.js
@@ -1,11 +1,14 @@
 const nav = document.querySelector('nav');
 const categories = document.querySelector('.categories');
+const overlay = document.querySelector('.overlay');
 
-document.querySelector('#burger-btn').addEventListener('click', show);
+document.querySelector('#burger-btn').addEventListener('click', showNav);
+overlay.addEventListener('click', showNav);
 
-function show() {
+function showNav() {
   nav.classList.toggle('active');
   categories.classList.remove('active');
+  overlay.classList.toggle('active');
   setTimeout(() => {categories.classList.toggle('d-none');}, 500);
 };
 
