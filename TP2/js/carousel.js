966cc9830232ccad5175e6b49171dbc64b40bbbe
Camilo Cagliolo <camilocagliolo14@gmail.com>
2023-10-10 21:42:11 -0300

    Avance en el carrousel

---
+++ TP2/js/carousel.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.carousel>button');
+
+buttons.forEach(button => {
+    if(button.classList.contains('previous')){
+
+    }
+    else if(button.classList.contains('next')){
+        
+    }
+})
