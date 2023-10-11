f752f1d1649df776e31c054fcb5ad4f38e2d654d
Camilo Cagliolo <ccagliolo@alumnos.exa.unicen.edu.ar>
2023-10-11 19:15:20 -0300

    Agregados carrouseles. Arreglado un bug que no permitía seguir el estado de cada uno por separado

--- TP2/js/carousel.js
+++ TP2/js/carousel.js
@@ -1,7 +1,14 @@
 const buttons = document.querySelectorAll('.carousel>button');
-let currentStep = 0;
+let currentStep = [];
 
-buttons.forEach(button => {
+buttons.forEach((button, index) => {
+    if(index % 2 == 0){
+        button.id = `button-${index/2}`;
+        currentStep[index/2] = 0;
+    } else {
+        button.id = `button-${(index-1)/2}`;
+    }
+
     if(button.classList.contains('previous')){
         button.addEventListener('click', moveBackward);
     }
@@ -12,48 +19,50 @@ buttons.forEach(button => {
 
 function moveForward(e){
     if(e.target.classList.contains('disabled')) return;
+    const index = e.target.id.split('-')[1];
     const carousel = e.target.parentElement;
     const previous = carousel.querySelector('.previous');
     const next = carousel.querySelector('.next');
     const cards = carousel.querySelectorAll('.carousel-body .card');
 
-    carousel.parentElement.querySelector(`#radio-${currentStep}`).classList.remove('active');
-    currentStep++;
-    carousel.parentElement.querySelector(`#radio-${currentStep}`).classList.add('active');
-
+    carousel.parentElement.querySelector(`.radio-${currentStep[index]}`).classList.remove('active');
+    currentStep[index]++;
+    carousel.parentElement.querySelector(`.radio-${currentStep[index]}`).classList.add('active');
+    
     cards.forEach(card => {
-        card.classList.add(`move-left-${currentStep}`);
+        card.classList.add(`move-left-${currentStep[index]}`);
     });
 
-    if(currentStep > 0){
+    if(currentStep[index] > 0){
         previous.classList.remove('disabled');
     }
 
-    if(currentStep >= cards.length/3 - 1){
+    if(currentStep[index] >= cards.length/3 - 1){
         next.classList.add('disabled');
     }
 }
 
 function moveBackward(e){
     if(e.target.classList.contains('disabled')) return;
+    const index = e.target.id.split('-')[1];
     const carousel = e.target.parentElement;
     const previous = carousel.querySelector('.previous');
     const next = carousel.querySelector('.next');
     const cards = carousel.querySelectorAll('.carousel-body .card');
     
     cards.forEach(card => {
-        card.classList.remove(`move-left-${currentStep}`);
+        card.classList.remove(`move-left-${currentStep[index]}`);
     });
 
-    carousel.parentElement.querySelector(`#radio-${currentStep}`).classList.remove('active');
-    currentStep--;
-    carousel.parentElement.querySelector(`#radio-${currentStep}`).classList.add('active');
+    carousel.parentElement.querySelector(`.radio-${currentStep[index]}`).classList.remove('active');
+    currentStep[index]--;
+    carousel.parentElement.querySelector(`.radio-${currentStep[index]}`).classList.add('active');
 
-    if(currentStep == 0){
+    if(currentStep[index] == 0){
         previous.classList.add('disabled');
     }
 
-    if(currentStep < 2){ 
+    if(currentStep[index] < 2){ 
         next.classList.remove('disabled');
     }
 }
