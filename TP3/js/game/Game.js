ff0204fef2c15020a94e0d74fb470527c194c8e5
Camilo Cagliolo <camilocagliolo14@gmail.com>
2023-10-30 01:25:52 -0300

    Falta resolver el tema del fondo con respecto a la ventana de los jugadores + drag de la ficha

--- TP3/js/game/Game.js
+++ TP3/js/game/Game.js
@@ -1,22 +1,59 @@
 import Player from './Player.js';
 import Board from './Board.js';
+import Disk from './Disk.js';
 class Game {
     constructor(ctx, config) {
         this.ctx = ctx
         this.config = config;
         this.players = {
-            player1: new Player(1, config.players[0].name, config.players[0].color, config.players[0].character, config.players[0].img, config.totalDisks),
-            player2: new Player(2, config.players[1].name, config.players[1].color, config.players[1].character, config.players[1].img, config.totalDisks)
+            player1: new Player(1, config.players[0].name, config.players[0].color, config.players[0].character, config.players[0].img, config.totalDisks, new Disk(0, 0, config.tileSize / 3, config.players[0].color)),
+            player2: new Player(2, config.players[1].name, config.players[1].color, config.players[1].character, config.players[1].img, config.totalDisks, new Disk(0, 0, config.tileSize / 3, config.players[1].color))
         };
         this.board = new Board(config.width / 2 - config.cols / 2 * config.tileSize, config.height / 2 - config.rows / 2 * config.tileSize, config.tileSize, config.rows, config.cols);
         this.currentPlayer = this.players.player1;
+        this.pile1 = null;
+        this.pile2 = null;
     }
 
     initGame() {
+        //Clears previous board
         this.ctx.clearRect(0, 0, this.config.width, this.config.height);
+        this.ctx.canvas.parentElement.querySelector('.player-info.p1')?.remove();
+        this.ctx.canvas.parentElement.querySelector('.player-info.p2')?.remove();
+        //Draws new board
         this.board.draw(this.ctx);
         this.players.player1.displayPlayerInfo(this.ctx, 1);
+        this.pile1 = this.players.player1.getPileCanvas();
         this.players.player2.displayPlayerInfo(this.ctx, 2);
+        this.pile2 = this.players.player2.getPileCanvas();
+        this.pile1.addEventListener('mousedown', (e) => {
+            if(this.currentPlayer !== this.players.player1) return;
+            this.players.player1.consumeDisk();
+            this.players.player1.updateDiskPile();
+            this.pile1.addEventListener('mousemove', (e) => this.moveDisk(e));
+            this.pile1.parentElement.addEventListener('mousemove', (e) => this.moveDisk(e));
+            this.ctx.canvas.addEventListener('mousemove', (e) => this.moveDisk(e));
+        }
+        );
+        this.pile2.addEventListener('mousedown', (e) => {
+            if(this.currentPlayer !== this.players.player2) return;
+            this.players.player2.consumeDisk();
+            this.players.player2.updateDiskPile();
+            this.pile2.addEventListener('mousemove', (e) => this.moveDisk(e));
+            this.pile2.parentElement.addEventListener('mousemove', (e) => this.moveDisk(e));
+            this.ctx.canvas.addEventListener('mousemove', (e) => this.moveDisk(e));
+        }
+        );
+    }
+
+    moveDisk(e) {
+        let x = e.clientX - this.ctx.canvas.getBoundingClientRect().left;
+        let y = e.clientY - this.ctx.canvas.getBoundingClientRect().top;
+        let disk = this.currentPlayer.getDisk();
+        disk.move(x, y);
+        this.ctx.clearRect(0, 0, this.config.width, this.config.height);
+        this.board.draw(this.ctx);
+        disk.draw(this.ctx);
     }
 
     getBoard() {
--- TP3/js/game/Player.js
+++ TP3/js/game/Player.js
@@ -1,5 +1,5 @@
 class Player {
-    constructor (playerNumber, name, color, character, image, totalDisks) {
+    constructor(playerNumber, name, color, character, image, totalDisks, disk) {
         this.playerNumber = playerNumber;
         this.name = name;
         this.character = character;
@@ -7,6 +7,8 @@ class Player {
         this.color = color;
         this.score = 0;
         this.totalDisks = totalDisks;
+        this.disk = disk;
+        this.subctx = null;
     }
 
     getName() {
@@ -29,18 +31,63 @@ class Player {
         this.score++;
     }
 
+    consumeDisk() {
+        this.totalDisks--;
+    }
+
+    restoreDisk() {
+        this.totalDisks++;
+    }
+
+    getDisk() {
+        return this.disk;
+    }
+
     displayPlayerInfo(ctx, playerNumber) {
         let canvas = ctx.canvas;
         let div = document.createElement('div');
         div.classList.add('player-info', `p${playerNumber}`);
         div.innerHTML = `
-            <img src="${this.image}" alt="${this.name}">
-            <span>${this.name}</span>
-            <span>${this.score}</span>
+            <div>
+                <h2>${this.score}</h2>
+                <div class="user-picture">
+                    <img src="${this.image}" alt="${this.name}">
+                </div>
+                <span>${this.name} (${this.character})</span>
+            </div>
+            <canvas></canvas>
         `;
         canvas.parentElement.appendChild(div);
+        this.subctx = div.querySelector('canvas').getContext('2d');
+        this.subctx.canvas.width = 2 * this.disk.radius;
+        this.updateDiskPile();
     }
 
+    updateDiskPile() {
+        let height = 10 * this.totalDisks;
+        this.subctx.canvas.height = height;
+        this.subctx.clearRect(0, 0, 2 * this.disk.radius, height);
+        this.subctx.strokeStyle = this.color;
+        this.subctx.lineWidth = 8;
+        for (let i = 0; i < this.totalDisks; i++) {
+            this.subctx.beginPath();
+            this.subctx.moveTo(0, 8 * i+4);
+            this.subctx.lineTo(2 * this.disk.radius, 8 * i+4);
+            this.subctx.stroke();
+        }
+        this.subctx.strokeStyle = 'white';
+        this.subctx.lineWidth = 1;
+        for (let i = 0; i < this.totalDisks; i++) {
+            this.subctx.beginPath();
+            this.subctx.moveTo(0, 1+8 * i);
+            this.subctx.lineTo(2 * this.disk.radius, 1+8 * i);
+            this.subctx.stroke();
+        }
+    }
+
+    getPileCanvas(){
+        return this.subctx.canvas;
+    }
 }
 
 export default Player;
