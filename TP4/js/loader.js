c5abc2836688da1b1ae7c199d37a1d0590f83373
Camilo Cagliolo <camilocagliolo14@gmail.com>
2023-11-27 23:48:09 -0300

    Agregado un porcentaje de carga

--- TP4/js/loader.js
+++ TP4/js/loader.js
@@ -2,11 +2,22 @@ const preventScroll = (e) => {
     e.preventDefault();
     e.stopPropagation();
     return false;
-  }
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     //Simula el tiempo de carga de la página
     document.querySelector('#loader-container').addEventListener('wheel', preventScroll, { passive: false });
+    let span = document.querySelector('#loader-container span');
+    const info = setInterval(() => {
+        let porc = parseInt(span.innerHTML.slice(0, -1));
+        if (porc >= 100) {
+            span.innerHTML = "100%";
+            clearInterval(info);
+        } else {
+            span.innerHTML = `${porc + 1}%`;
+        }
+
+    }, 30)
     setTimeout(function () {
         document.getElementById("loader-container").style.display = "none";
     }, 5000);
